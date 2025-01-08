fc71e76d65913ab86013cd8321eac0bc291381cf
Wylie Fisher <me@wyliefisher.com>
2025-01-07 21:17:43 -0500

    first commit!
    - adding all the needed files: HTML, CSS, JS, and JSON
    - some basic styling to make things look better
    - some basic sorting functionality, thanks JS!
    - a first pass at the data

---
+++ script.js
@@ -0,0 +1,33 @@
+async function fetchData() {
+  const response = await fetch('./data.json');
+  const data = await response.json();
+  return data;
+}
+
+async function sortByReleaseDate() {
+  const data = await fetchData();
+  const sortedData = data.sort((a, b) => new Date(a.releaseDate) - new Date(b.releaseDate));
+  displayData(sortedData);
+}
+
+async function sortByDate() {
+  const data = await fetchData();
+  const sortedData = data.sort((a, b) => new Date(a.byDate) - new Date(b.byDate));
+  displayData(sortedData);
+}
+
+const displayData = (data) => {
+  const container = document.getElementById('starwars');
+  container.innerHTML = '';
+  data.forEach(item => {
+    const div = document.createElement('div');
+    div.className = `sw-item ${item.type}`;
+    div.innerHTML = `<p>${item.title}</p><p>${item.releaseDate}</p>`;
+    container.appendChild(div);
+  });
+};
+
+document.getElementById('release').addEventListener('click', sortByReleaseDate);
+document.getElementById('chronological').addEventListener('click', sortByDate);
+
+fetchData().then(displayData);
