0a66a786d3d7637c1877692fbc03af75936ec88b
Wylie Fisher <me@wyliefisher.com>
2025-01-09 13:23:51 -0500

    - adding filtering - cleaning up the type so it shows a title and not the slug - clean up the date to remove the negative symbol and change the display if we're at 0BBY - add a checbox check so we can turn off filtering so the CSS shows everything when no checkboxes are checked

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
 // create the visual display for the data
 function itemCard(item) {
+  // replace slugs with nicer titles
+  let type = "";
+  switch(item.type) {
+    case "movie":
+      type = "Movie";
+      break;
+    case "tvshow":
+      type = "TV Show";
+      break;
+    case "comic":
+      type = "Comic";
+      break;
+    case "book":
+      type = "Book";
+      break;
+    case "videogame":
+      type = "Video Game";
+      break;
+  }
+  // remove negative sign from the BBY date
+  let bbyDate = item.chronoDate;
+  if(item.chronoDate < 0) {
+    bbyDate = bbyDate * -1;
+  }
+  // format date nicer
+  let byDate = "";
+  switch(item.chronoDate > 0) {
+    case true:
+      byDate = `<strong>ABY</strong>: ${item.chronoDate} standard years`;
+      break;
+    case false:
+      byDate = `<strong>BBY</strong>: ${bbyDate} standard years`;
+      break;
+    default:
+      byDate = `<strong>BBY</strong>: ${bbyDate}`;
+  }
+  // return the card
   return (`
-    <p>${item.type}</p>
-    <p>${item.title}</p>
-    <p>Era: ${item.era}</p>
-    <p>Release Year: ${item.releaseDate}</p>
-    <p>${item.byDate >= 0 ? "ABY: " : "BBY: "}${item.byDate}</p>
+    <div class="card">
+      <p><strong>${type}</strong></p>
+      <p>${item.title}</p>
+      <p><strong>Era</strong>: ${item.era}</p>
+      <p><strong>Release Year</strong>: ${item.releaseDate}</p>
+      <p>${byDate}</p>
+    </div>
   `)
 };
 
@@ -16,6 +55,7 @@ async function fetchData() {
   return data;
 }
 
+// TODO: consolidate sortByReleaseDate and sortByDate functions?
 // sort the data by release date
 async function sortByReleaseDate() {
   const data = await fetchData();
@@ -26,8 +66,33 @@ async function sortByReleaseDate() {
 // sort the data by BBY/ABY date
 async function sortByDate() {
   const data = await fetchData();
-  const sortedData = data.sort((a, b) => new Date(a.byDate) - new Date(b.byDate));
+  const sortedData = data.sort((a, b) => new Date(a.chronoDate) - new Date(b.chronoDate));
   displayData(sortedData);
+}
+
+// check the checkboxes
+function checkboxCheck() {
+  const starwarsElement = document.getElementById('starwars');
+  const checkboxes = document.querySelectorAll('.checkbox');
+  let checked = [];
+  checkboxes.forEach(function(elem) {
+    checked.push(elem.checked);
+  });
+  // TODO: refactor to be cleaner
+  if(!checked.includes(true)) {
+    starwarsElement.classList.remove('filtered');
+  } else {
+    starwarsElement.classList.add('filtered');
+  }
+}
+
+// do the filtering
+function filterData(data) {
+  const swItem = document.getElementsByClassName(`sw-item ${data}`);
+  checkboxCheck();
+    Array.from(swItem).forEach(item => {
+      item.classList.toggle("visible");
+    });
 }
 
 // display the data on the page
@@ -46,5 +111,11 @@ const displayData = (data) => {
 document.getElementById('release').addEventListener('click', sortByReleaseDate);
 document.getElementById('chronological').addEventListener('click', sortByDate);
 
+document.getElementById('movie').addEventListener('click', () => filterData("movie"));
+document.getElementById('tvshow').addEventListener('click', () => filterData("tvshow"));
+document.getElementById('book').addEventListener('click', () => filterData("book"));
+document.getElementById('comic').addEventListener('click', () => filterData("comic"));
+document.getElementById('videogame').addEventListener('click', () => filterData("videogame"));
+
 // initial call to fetch and display the data
-fetchData().then(sortByReleaseDate);
+fetchData().then(sortByDate);
