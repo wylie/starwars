895d3601b2ffba2f32203c2bf18aa2102dd6bf42
Wylie Fisher <me@wyliefisher.com>
2025-01-12 14:05:22 -0500

    oh boy, a lot of work today on the sorting and filtering.
    we can now filter, then sort, but we do have a bug.
    
    after you filter and sort, then add another filter, they aren't in order, they show up at the bottom of the list. so need to fix that. I made a TODO for that

--- script.js
+++ script.js
@@ -28,13 +28,13 @@ function itemCard(item) {
   let byDate = "";
   switch(item.chronoDate > 0) {
     case true:
-      byDate = `<strong>ABY</strong>: ${item.chronoDate} standard years`;
+      byDate = `<strong>ABY:&nbsp;</strong>${item.chronoDate} standard years`;
       break;
     case false:
-      byDate = `<strong>BBY</strong>: ${bbyDate} standard years`;
+      byDate = `<strong>BBY:&nbsp;</strong>${bbyDate} standard years`;
       break;
     default:
-      byDate = `<strong>BBY</strong>: ${bbyDate}`;
+      byDate = `<strong>BBY:&nbsp;</strong>${bbyDate}`;
   }
   // return the card
   return (`
@@ -44,13 +44,13 @@ function itemCard(item) {
         <h3>${item.title}</h3>
       </div>
       <div class="card-meta">
-        <p><strong>Era</strong>: ${item.era}</p>
-        ${item.pages ? `<p><strong>Pages</strong>: ${item.pages}</p>` : ""}
-        ${item.category ? `<p><strong>Category</strong>: ${item.category}</p>` : ""}
-        ${item.platforms ? `<p><strong>Platforms</strong>: ${item.platforms}</p>` : ""}
-        ${item.director ? `<p><strong>Director</strong>: ${item.director}</p>` : ""}
-        ${item.developer ? `<p><strong>Developer</strong>: ${item.developer}</p>` : ""}
-        <p><strong>Release Year</strong>: ${item.releaseDate}</p>
+        <p><strong>Era:&nbsp;</strong>${item.era}</p>
+        ${item.pages ? `<p><strong>Pages:&nbsp;</strong>${item.pages}</p>` : ""}
+        ${item.category ? `<p><strong>Category:&nbsp;</strong>${item.category}</p>` : ""}
+        ${item.platforms ? `<p><strong>Platforms:&nbsp;</strong>${item.platforms}</p>` : ""}
+        ${item.director ? `<p><strong>Director:&nbsp;</strong>${item.director}</p>` : ""}
+        ${item.developer ? `<p><strong>Developer:&nbsp;</strong>${item.developer}</p>` : ""}
+        <p><strong>Release Year:&nbsp;</strong>${item.releaseDate}</p>
         <p>${byDate}</p>
       </div>
     </div>
@@ -58,86 +58,79 @@ function itemCard(item) {
 };
 
 // fetch the data from the json file
+let data = [];
 async function fetchData() {
-  const response = await fetch('./data.json');
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch('./data.json');
+    const jsonData = await response.json();
+    
+    data = [...data, ...jsonData];
+    // console.log('Fetched data:', data); // Log fetched data
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
 
 // check the checkboxes
 function checkboxCheck() {
-  const starwarsElement = document.getElementById('starwars');
-  const checkboxes = document.querySelectorAll('.checkbox');
   let checked = [];
+  const checkboxes = document.querySelectorAll('.checkbox');
   checkboxes.forEach(function(elem) {
-    checked.push(elem.checked);
+    checked.push({ id: elem.id, checked: elem.checked });
   });
-  if(!checked.includes(true)) {
-    starwarsElement.classList.remove('filtered');
-  } else {
-    starwarsElement.classList.add('filtered');
-  }
+  data.forEach(item => {
+    const checkbox = checked.find(elem => elem.id === item.type);
+    item.visible = checkbox ? checkbox.checked : false;
+  });
+  drawPage(data);
+  return checked;
 }
 
-// do the filtering
-let filteredData = [];
-function filterData(filterBy) {
-  const swItem = document.getElementsByClassName(`sw-item ${filterBy}`);
-  checkboxCheck();
-  Array.from(swItem).forEach(item => {
-    item.classList.toggle("visible");
+// sort the data by release or chronological date
+// TODO: when you filter, then sort, then add another filter, the new
+// data is not sorted correctly, it's applied to the end of the list
+function sortData(sortBy) {
+  checkboxCheck(); // Ensure visibility is updated before sorting
+  const sortedData = data.filter(item => item.visible).sort((a, b) => {
+    if (sortBy === 'releaseDate') {
+      return new Date(a[sortBy]) - new Date(b[sortBy]);
+    } else {
+      return a[sortBy] - b[sortBy];
+    }
   });
-  filteredData.push(swItem);
+  drawPage(sortedData);
 }
-
-// sort the data by release date or chronological date
-async function sort(sortBy) {
-  let data = "";
-  const starwarsElement = document.getElementById('starwars').classList.contains('filtered');
-  if (starwarsElement) {
-    displayData(filteredData);
-    const sortedData = filteredData.sort((a, b) => {
-      if (sortBy === 'releaseDate') {
-        return new Date(a[sortBy]) - new Date(b[sortBy]);
-      } else {
-        return a[sortBy] - b[sortBy];
-      }
-    });
-    displayData(sortedData);
-  } else {
-    data = await fetchData();
-    console.log(data);
-    const sortedData = data.sort((a, b) => {
-      if (sortBy === 'releaseDate') {
-        return new Date(a[sortBy]) - new Date(b[sortBy]);
-      } else {
-        return a[sortBy] - b[sortBy];
-      }
-    });
-    displayData(sortedData);
-  }
-}    
 
 // display the data on the page
-const displayData = (data) => {
+const drawPage = (data) => {
   const starwarsElement = document.getElementById('starwars');
   starwarsElement.innerHTML = '';
   data.forEach(item => {
     const div = document.createElement('div');
-    div.className = `sw-item ${item.type}`;
+    div.className = `sw-item ${item.type} ${item.visible ? 'visible' : ''}`;
     div.innerHTML = itemCard(item);
     starwarsElement.appendChild(div);
   });
 };
 
-// add event listeners
-document.getElementById('release').addEventListener('click', () => sort("releaseDate"));
-document.getElementById('chronological').addEventListener('click', () => sort("chronoDate"));
-document.getElementById('movie').addEventListener('click', () => filterData("movie"));
-document.getElementById('tvshow').addEventListener('click', () => filterData("tvshow"));
-document.getElementById('book').addEventListener('click', () => filterData("book"));
-document.getElementById('comic').addEventListener('click', () => filterData("comic"));
-document.getElementById('videogame').addEventListener('click', () => filterData("videogame"));
-
 // initial call to fetch and display data
-fetchData().then(() => sort("chronoDate"));
+fetchData().then(() => {
+  // read from the HTML so we don't have to change this in two places
+  let sortBy = "";
+  const radios = document.querySelectorAll('.radio');
+  radios.forEach(radio => {
+    if(radio.checked) {
+      sortBy = radio.id;
+    }
+  });
+  sortData(sortBy);
+  // event listeners
+  document.getElementById('releaseDate').addEventListener('change', () => sortData("releaseDate"));
+  document.getElementById('chronoDate').addEventListener('change', () => sortData("chronoDate"));
+  document.getElementById('movie').addEventListener('change', () => checkboxCheck("movie"));
+  document.getElementById('tvshow').addEventListener('change', () => checkboxCheck("tvshow"));
+  document.getElementById('book').addEventListener('change', () => checkboxCheck("book"));
+  document.getElementById('comic').addEventListener('change', () => checkboxCheck("comic"));
+  document.getElementById('videogame').addEventListener('change', () => checkboxCheck("videogame"));
+});
